7f8c1afc3f88f49abea20a1ad543894e437dca00
the-man2004 <heinrichhb111@gmail.com>
2024-01-04 13:45:03 -0800

    Added & styled background image & title.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Home</h1>
+  <div class="hero">
+    <h1 class="hero-title">Delicious Foods</h1>
+  </div>
 </template>
+
+<style scoped>
+.hero {
+  height: 25vw;
+  min-height: 10rem;
+  max-height: 25rem;
+  background-image: url("../assets/hero-image.jpg");
+  background-size: cover;
+  background-position: center;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hero-title {
+  margin: 0;
+  color: var(--accent-color);
+  font-family: "Playball";
+  font-size: clamp(2.5rem, 10vw, 7rem);
+  text-shadow: 3px 3px 3px var(--primary-color);
+}
+</style>
