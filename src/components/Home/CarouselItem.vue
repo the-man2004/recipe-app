91c27aa79651b453e874ac0ff9a0f11e50b4066e
the-man2004 <heinrichhb111@gmail.com>
2024-01-11 11:38:44 -0800

    Now showing recipes in a slideshow.

--- src/components/Home/CarouselItem.vue
+++ src/components/Home/CarouselItem.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
     <img :src="props.recipe.strMealThumb" alt="" />
+    <p>Hello</p>
   </li>
 </template>
 
@@ -13,12 +14,15 @@ li {
   list-style: none;
   display: block;
   width: calc(25vw - 24px);
+
+  transition: all 500ms ease;
 }
 
 img {
   display: block;
   width: calc(25vw - 24px);
   height: auto;
+  aspect-ratio: 1 / 1;
 }
 
 @media (min-width: calc(80rem + 96px)) {
--- src/components/Home/TheCarousel.vue
+++ src/components/Home/TheCarousel.vue
@@ -5,22 +5,60 @@
         v-for="recipe in recipes"
         :recipe="recipe"
         :key="recipe.idMeal"
+        ref="listOfRecipes"
+        class="carouselItem"
       />
     </ul>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRecipeStore } from "../../stores/useRecipeStore";
 import CarouselItem from "./CarouselItem.vue";
 
 const recipeStore = useRecipeStore();
 
 const recipes = computed(() => recipeStore.miscellaneousRecipes);
+
+const listOfRecipes = ref([]);
+
+onMounted(() => {
+  let num = 100;
+
+  setInterval(() => {
+    const recipes = document.querySelectorAll(".carouselItem");
+
+    const maxMoves = recipes.length - 3;
+
+    if (num / 100 === maxMoves) {
+      num = 100;
+      recipes.forEach((recipe) => {
+        // recipe.style.border = "2px solid red";
+        recipe.style.transform = `translateX(0)`;
+      });
+    } else {
+      recipes.forEach((recipe) => {
+        // recipe.style.border = "2px solid red";
+        recipe.style.transform = `translateX(-${num}%)`;
+      });
+      num += 100;
+    }
+    // setTimeout(() => {
+    //   recipes.forEach((recipe) => {
+    //     recipe.style.border = "2px solid green";
+    //     recipe.style.transform = `translateX(-${num}%)`;
+    //   });
+    // }, 2000);
+  }, 3000);
+});
 </script>
 
 <style scoped>
+.test {
+  color: white;
+}
+
 .carousel-container {
   margin: 1.5rem 3rem;
   display: none;
@@ -35,7 +73,7 @@ const recipes = computed(() => recipeStore.miscellaneousRecipes);
 
   display: flex;
 
-  overflow-x: scroll;
+  overflow-x: hidden;
 }
 
 @media (min-width: 60rem) {
