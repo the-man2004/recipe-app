dd4507976767f94f1797a095aa40f1d6bac975a4
the-man2004 <heinrichhb111@gmail.com>
2024-01-15 16:14:17 -0800

    Added bread crumbs.

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,19 +1,57 @@
 <template>
   <basic-hero />
-  <div class="recipes-container">
-    <h2>Recipes</h2>
-    <p>{{ props.id }}</p>
+  <div class="recipes">
+    <div class="bread-crumbs">
+      <span>
+        <router-link to="/">Home</router-link>
+      </span>
+      >> <span>Recipes</span> >> <span>{{ category }}</span>
+    </div>
+    <div class="recipes-container">
+      <h2>Recipes</h2>
+      <p>{{ category }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
 import BasicHero from "../components/BasicHero.vue";
 
 const props = defineProps(["id"]);
+
+const category = computed(
+  () => props.id.charAt(0).toUpperCase() + props.id.slice(1)
+);
 </script>
 
 <style scoped>
+.recipes {
+  margin: 0 auto;
+  padding: 0 1rem;
+  max-width: 80rem;
+}
+
 .recipes-container {
-  margin-top: 4rem;
+}
+
+.bread-crumbs {
+  color: var(--accent-color);
+}
+
+.bread-crumbs a {
+  text-decoration: none;
+  font-weight: 700;
+  color: var(--accent-color);
+}
+
+span {
+  font-size: 1.2em;
+}
+
+@media (min-width: 40rem) {
+  .bread-crumbs {
+    font-size: 1.3rem;
+  }
 }
 </style>
