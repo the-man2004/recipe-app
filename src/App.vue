8d669fb39463bf9117f6e395c21dba14d55658d1
the-man2004 <heinrichhb111@gmail.com>
2024-01-24 15:34:29 -0800

    Added & styled footer.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <TheHeader />
   <router-view></router-view>
+  <TheFooter />
 </template>
 
 <script setup>
 import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,58 @@
+<template>
+  <footer>
+    <div>
+      <router-link to="/" class="home-link">Delicious Foods</router-link>
+      <div class="social-links">
+        <a href="https://facebook.com">
+          <i class="fa-brands fa-facebook link"></i>
+        </a>
+        <a href="https://instagram.com">
+          <i class="fa-brands fa-instagram link"></i>
+        </a>
+        <a href="https://pinterest.com">
+          <i class="fa-brands fa-pinterest link"></i>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  margin: 2rem 0 0 0;
+  background: var(--dark-color);
+}
+
+footer div {
+  margin: 0 auto;
+  padding: 2rem 1rem;
+  max-width: 80rem;
+}
+
+a {
+  font-family: "Playball", sans-serif;
+  text-decoration: none;
+}
+
+.home-link {
+  margin: 0;
+  color: var(--accent-color);
+  font-family: "Playball";
+  font-size: 2.5rem;
+  text-shadow: 3px 3px 3px var(--primary-color);
+}
+
+.social-links {
+  margin-top: 1rem;
+  padding: 0;
+
+  display: flex;
+  gap: 0.5rem;
+
+  font-size: 1.2rem;
+}
+
+.social-links a {
+  color: var(--secondary-color);
+}
+</style>
