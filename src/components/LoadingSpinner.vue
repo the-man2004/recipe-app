e5d6192cd9e594fc06df6d9a7e660e06c679f2b8
the-man2004 <heinrichhb111@gmail.com>
2024-01-29 15:13:15 -0800

    Added loading spinner animation.

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <span class="loader"></span>
     <p>Loading...</p>
   </div>
 </template>
@@ -7,6 +8,28 @@
 <style scoped>
 div {
   display: flex;
+  align-items: center;
   justify-content: center;
+  flex-direction: column;
+}
+
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #fff;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
